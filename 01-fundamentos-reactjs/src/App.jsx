41f333ef2832da5209a3147d7b53150b1cc01ba3
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-07-20 21:11:46 -0400

    SideBar criada e estilizada (sem ícones)

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,32 @@
+// Importação de componentes
 import { Post } from './Post'
 import {Header} from './components/Header'
+import {Sidebar} from './components/Sidebar'
+
+// Importação de estilização
+import styles from './App.module.css'
+
+// Importação de estilização global
 import './global.css'
 
 export function App() {
   return (
     <>
       <Header/>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Pedro Nogueira" 
+            content="Salve salve rapaziada do Ignite. Começando agora os estudos de React.js. Quero aprender a utilizar TypeScript!"
+          />
+          <Post
+            author="Mayk Brito"
+            content="Galera, vocês precisam conhecer o Explorer!"
+          />
+        </main>
+      </div>
 
-      <Post 
-        author="Pedro Nogueira" 
-        content="Salve salve rapaziada do Ignite. Começando agora os estudos de React.js. Quero aprender a utilizar TypeScript!"
-      />
-      <Post
-        author="Mayk Brito"
-        content="Galera, vocês precisam conhecer o Explorer!"
-      />
     </>
   )
 }
