18e03b59386493b42c37bc71de1a841401e186f1
João Pedro Nogueira <jpedronogueira.dev@gmail.com>
2022-07-26 22:57:43 -0400

    Extensão de interface configurada no Avatar

--- 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
@@ -1,15 +1,15 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css'
 
-interface propsType {
-  src: string
+interface propsType extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean
 }
 
-export function Avatar({ hasBorder = true, src }: propsType) {
+export function Avatar({ hasBorder = true, ...props }: propsType) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
+      {...props}
     />
   );
 }
